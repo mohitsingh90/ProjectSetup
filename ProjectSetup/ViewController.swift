9a28d5d09c19799dbb4e22b0d796e4ec697e704a
Mohit <mohitsingh@outlook.in>
2018-10-04 12:04:38 +0530

    Initial Commit

---
+++ ProjectSetup/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProjectSetup
+//
+//  Created by MOHIT SINGH on 04/10/18.
+//  Copyright © 2018 MOHIT SINGH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
